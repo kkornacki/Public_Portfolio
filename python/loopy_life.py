eb27403c775917d549ccf778c0cc328fd19a500f
Keith Kornacki <keith.kornacki@outlook.com>
2024-11-03 00:53:41 -0400

    Working Backup

--- python/loopy_life.py
+++ python/loopy_life.py
@@ -1,3 +1,5 @@
+import datetime
+
 run_program = True
 last_command = ''
 
@@ -30,9 +32,24 @@ def process_input(input_param):
     elif input_value == "calculate difference":
         calculate_difference()
         last_command = "calculate difference"
+    elif input_value == "calculate product":
+        calculate_product()
+        last_command = "calculate product"
     elif input_value == "calculate average":
         calculate_average()
         last_command = "calculate average"
+    elif input_value == "calculate min":
+        calculate_min()
+        last_command = "calculate min"
+    elif input_value == "calculate max":
+        calculate_max()
+        last_command = "calculate max"
+    elif input_value == "current date":
+        get_current_date()
+        last_command = "current date"
+    elif input_value == "determine equality":
+        determine_equality()
+        last_command = "determine equality"
     elif input_value == "exit" or input_value == "end" or input_value == "close":
         exit()
     elif input_value == "repeat":
@@ -108,4 +125,47 @@ def calculate_average():
 
     print(f"The average of {float_array} is {average: .2f}")
     return
+
+def calculate_min():
+    input_value_string= input("Enter the values for which you want to calculate the minimum (lowest) value: ")
+    input_values = input_value_string.split()
+    float_array = [float(number) for number in input_values]
+ 
+    result = min(float_array)
+
+    print(f"The lowest value of the data set {float_array} is {result}")
+    return
+
+def calculate_max():
+    input_value_string= input("Enter the values for which you want to calculate the max (highest) value: ")
+    input_values = input_value_string.split()
+    float_array = [float(number) for number in input_values]
+ 
+    result = max(float_array)
+
+    print(f"The highest value of the data set {float_array} is {result}")
+    return
+
+def calculate_product():
+    param_1 = input("Enter the first parameter: ")
+    param_2 = input("Enter the second parameter: ")
+    result = (float(param_1) * float(param_2))
+
+    print(f"The product of {param_1} * {param_2} is {result}")
+    return
+
+
+def get_current_date():
+    exact_date_time = datetime.datetime.now()
+    print(exact_date_time.strftime("%x"))
+    return
+
+def determine_equality():
+    param_1 = input("Enter the first parameter: ")
+    param_2 = input("Enter the second parameter: ")
+    result = param_1 == param_2
+
+    print(f"{param_1} == {param_2} equality check evaluates to: {result}")
+    return
+
 program_start()
